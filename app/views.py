a2a201431e1563065fc5717a0d02c14140e93662
unknown <me@ajheflin.com>
2021-06-16 13:47:36 -0400

    Froze app

--- app/views.py
+++ app/views.py
@@ -1,9 +1,20 @@
 from app import app
 from flask import render_template
+from math import floor
+import sys
+
+pages = ["index", "resan", "rescat"]
+readable = ["Home", "Resume Analyzer", "Resume Catalog"]
 
 @app.route("/")
+def home():
+	return render_template("index.html", pages=enumerate(pages), readable=readable, col_width=floor(8 / len(pages)), selected="index")
+@app.route("/index/")
 def index():
-	return render_template("index.html")
-@app.route("/resan")
+	return home()
+@app.route("/resan/")
 def resan():
-	return "Resume Analyzer"
+	return render_template("resan.html", pages=enumerate(pages), readable=readable, col_width=floor(8 / len(pages)), selected="resan")
+@app.route("/rescat/")
+def rescat():
+	return render_template("rescat.html", pages=enumerate(pages), readable=readable, col_width=floor(8 / len(pages)), selected="rescat")
